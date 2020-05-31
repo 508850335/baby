adf0df52ebf7f3e1db74b9b18335ab4c52e801e7
Balloon <33593535+508850335@users.noreply.github.com>
2020-06-01 07:07:47 +0800

    Update data.js

--- photo/data.js
+++ photo/data.js
@@ -1,46 +1,27 @@
 var data = [];
 
-var dataStr = "1.DeathNote<br>\
+var dataStr = "001<br>\
 <br>\
 Japanese Cartoon<br>\
 <br>\
 <br>\
-2.FateUBW<br>\
+002<br>\
 <br>\
 Japanese Cartoon<br>\
 <br>\
 <br>\
-3.FateZero<br>\
+003<br>\
 <br>\
 Japanese Cartoon<br>\
 <br>\
 <br>\
-4.GoneGirl<br>\
-<br>\
-American Movie<br>\
-<br>\
-<br>\
-5.Shameless<br>\
-<br>\
-American TV Series<br>\
-<br>\
-<br>\
-6.theDarkKnight<br>\
-<br>\
-American Movie<br>\
-<br>\
-<br>\
-7.ToyStory<br>\
-<br>\
-American Movie<br>\
 "
 var d = dataStr.split("<br><br><br>");
 for(var i = 0; i<d.length; i++){
   var c = d[i].split("<br><br>");
   data.push({
-    img: c[0]+ ".jpg",
+    img: c[0]+ ".JPG",
     caption: c[0].split(".")[1],
     desc: c[1]
   });
 }
-
