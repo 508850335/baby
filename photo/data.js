54f776c9119723c0b853a87afbfd02f0b230b67b
Balloon <33593535+508850335@users.noreply.github.com>
2020-06-01 07:28:04 +0800

    Update data.js

--- photo/data.js
+++ photo/data.js
@@ -1,6 +1,6 @@
 var data = [];
 
-var dataStr = "3.001<br><br>宝贝<br><br><br>2.002<br><br>宝贝<br><br><br>3.003<br><br>宝贝"
+var dataStr = "1.001<br><br>宝贝<br><br><br>2.002<br><br>宝贝<br><br><br>3.003<br><br>宝贝"
 var d = dataStr.split("<br><br><br>");
 for(var i = 0; i<d.length; i++){
   var c = d[i].split("<br><br>");
